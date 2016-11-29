d2053f2c5dd15a6625a26d2d63d4f575f98248b6
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-11-29 12:05:53 -0800

    added network settings configuration for the rails API

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# create_table "freelance_documents", force: :cascade do |t|
+#     t.string   "title"
+#     t.string   "description"
+#     t.text     "file_url"
+#     t.text     "image_url"
+#     t.datetime "created_at",  null: false
+#     t.datetime "updated_at",  null: false
+#   end
+
+10.times do |d|
+FreelanceDocument.create!(
+  title: "Document #{d}", 
+  description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+  Aliquam eleifend vehicula lorem, a convallis diam imperdiet et. Curabitur cursus egestas efficitur",
+  file_url: "https://www.adobe.com/content/dam/Adobe/en/products/acrobat/pdfs/adobe-acrobat-xi-create-form-or-template-tutorial_ue.pdf",
+  image_url:"https://static.pexels.com/photos/2394/lights-clouds-dark-car.jpg"
+  )
+end
